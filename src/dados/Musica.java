6c00e9f35152dd79c82317c877e291a126c21ade
Eduardo Camana <camana98@gmail.com>
2024-06-11 10:49:31 -0300

    Correção classes Musica e Video

--- src/dados/Musica.java
+++ src/dados/Musica.java
@@ -5,7 +5,12 @@ public class Musica extends Midia {
 	private double duracao;
 
 	public Musica(int codigo, String titulo, int ano, Categoria categoria, double duracao) {
+        super(codigo, titulo, ano, categoria);
+		this.duracao = duracao;
+    }
 
+	@Override
+	public double calculaLocacao() {
+		return 0;
 	}
-
 }
--- src/dados/Video.java
+++ src/dados/Video.java
@@ -5,7 +5,12 @@ public class Video extends Midia {
 	private int qualidade;
 
 	public Video(int codigo, String titulo, int ano, Categoria categoria, int qualidade) {
+        super(codigo, titulo, ano, categoria);
+		this.qualidade = qualidade;
+    }
 
+	@Override
+	public double calculaLocacao() {
+		return 0;
 	}
-
 }
